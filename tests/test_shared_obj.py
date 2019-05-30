1770563ff5c9c30a952b6e399fd253782db84133
Danny Price <dan@thetelegraphic.com>
2019-05-30 12:04:13 +1000

    Added print parenthesis (Py3)

--- tests/test_shared_obj.py
+++ tests/test_shared_obj.py
@@ -4,4 +4,4 @@ from pkg_resources import resource_filename
 DATAPATH = os.path.dirname(resource_filename("pyymw16", "spiral.txt"))
 gl, gb, dist, dm_host, ndir, mode_id, vbs, txt = 204.0, -6.5, 2000, 0, 2, -1, 0, ''
 a = ymw16.dmdtau(gl, gb, dist, dm_host, ndir, mode_id, vbs, DATAPATH, txt)
-print a
+print(a)
