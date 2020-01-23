d442884c95541df2276872859ada93b74cc13531
Danny Price <dan@thetelegraphic.com>
2020-01-23 16:37:23 +1100

    Updating tests for YT2020

--- tests/test_healpix_map.py
+++ tests/test_healpix_map.py
@@ -10,10 +10,18 @@ def test_healpix_map():
     assert np.isclose(hpmap[0], 19.607964)
     assert np.isclose(hpmap[1729], 29.772997)
 
+    hpmap = pygedm.generate_healpix_dm_map(nside=2, method='yt2020')
+    assert np.isclose(hpmap[0], 35.190342)
+
+    hpmap = pygedm.generate_healpix_dm_map(nside=2, method='yt2020_analytic')
+    assert np.isclose(hpmap[0], 35.01475)
+
     # Check that error is raised if healpy isn't installed
     with pytest.raises(RuntimeError):
         pygedm.HAS_HEALPIX = False
         pygedm.generate_healpix_dm_map(dist=30000, nside=32)
+
+
 
 if __name__ == "__main__":
     test_healpix_map()
