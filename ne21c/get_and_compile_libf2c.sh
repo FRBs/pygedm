62b4c42cd71ecb4a67f08fc80c70f5afdae718ab
Danny Price <dan@thetelegraphic.com>
2021-07-05 16:55:16 +0800

    Updating get_and_compile libf2c.a for manywheels action

--- ne21c/get_and_compile_libf2c.sh
+++ ne21c/get_and_compile_libf2c.sh
@@ -1,6 +1,6 @@
-# Download and attempt to compile libf2c.a 
+# Download and attempt to compile libf2c.a
 wget http://www.netlib.org/f2c/libf2c.zip
 unzip libf2c.zip
 mv makefile.u Makefile
 make 
-echo cp libf2c.a /usr/lib64/
+cp libf2c.a /usr/lib64/
