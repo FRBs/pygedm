e150e31176f11aa45ec539387ee58edda25d8222
Danny Price <dan@thetelegraphic.com>
2021-07-06 11:06:05 +0800

    The libf2c saga continues -- working on macOS again

--- ne21c/get_and_compile_libf2c.sh
+++
@@ -1,7 +0,0 @@
-# Download and attempt to compile libf2c.a
-wget http://www.netlib.org/f2c/libf2c.zip
-unzip libf2c.zip
-mv makefile.u Makefile
-sed -i 's/CFLAGS = -O/CFLAGS = -O -fPIC/' Makefile
-make 
-cp libf2c.a /usr/lib64/
